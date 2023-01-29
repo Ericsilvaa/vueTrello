ae1cb2e1b4edd8a1672878c0edad3316540cbcf8
Eric Oliveira <eriicsilva28@gmail.com>
2023-01-29 15:57:54 -0300

    init project

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <AppHeader />
+
+    <router-view />
+
+    <AppFooter />
+  </div>
+</template>
+<script>
+import AppHeader from "@/components/AppHeader.vue";
+import AppFooter from "@/components/AppFooter.vue";
+
+export default {
+  components: {
+    AppHeader,
+    AppFooter,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+
+  text-align: center;
+}
+</style>
---
+++ src/components/BoardCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <span>{{ name }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "board-card",
+  props: {
+    id: String,
+    name: String,
+  },
+};
+</script>
+
+<style></style>
